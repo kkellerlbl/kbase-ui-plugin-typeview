581d065ff171d86196550f233192bfe1c519f23d
Erik Pearson <eapearson@lbl.gov>
2019-04-16 17:15:40 -0700

    integration test support

--- src/plugin/iframe_root/modules/moduleViewPanel.js
+++ src/plugin/iframe_root/modules/moduleViewPanel.js
@@ -56,12 +56,15 @@ define([
                 });
 
                 return Promise.all([
-                    workspace.callFunc('get_module_info', [moduleParam]).spread((x) => {return x;}),
-                    workspace.callFunc('list_module_versions', [{mod: name}]).spread((x) => {return x;})
-                ])
-                    .spread((moduleInfo, moduleVersions) => {
-                        return [name, moduleInfo, moduleVersions];
-                    });
+                    workspace.callFunc('get_module_info', [moduleParam]).spread((x) => {
+                        return x;
+                    }),
+                    workspace.callFunc('list_module_versions', [{ mod: name }]).spread((x) => {
+                        return x;
+                    })
+                ]).spread((moduleInfo, moduleVersions) => {
+                    return [name, moduleInfo, moduleVersions];
+                });
             });
         }
 
@@ -74,122 +77,127 @@ define([
             container.style['flex-direction'] = 'column';
             container.style.flex = '1 1 0px';
             container.style.margin = '0 10px';
+            container.setAttribute('data-k-b-testhook-plugin', 'typeview');
         }
 
         function start(params) {
-            return loadData(params.moduleid)
-                // NOTE: Annoying but true, the module info does not contain the module name.
-                .spread((moduleName, moduleInfo, moduleVersions) => {
-                    const title = [
-                        'Module Specification for',
-                        span({ style: { textDecoration: 'underline' } }, params.moduleid)
-                    ].join(' ');
+            return (
+                loadData(params.moduleid)
+                    // NOTE: Annoying but true, the module info does not contain the module name.
+                    .spread((moduleName, moduleInfo, moduleVersions) => {
+                        const title = [
+                            'Module Specification for',
+                            span({ style: { textDecoration: 'underline' } }, params.moduleid)
+                        ].join(' ');
 
-                    runtime.send('ui', 'setTitle', title);
+                        runtime.send('ui', 'setTitle', title);
 
-                    const tabs = [
-                        {
-                            id: 'overview',
-                            tab: {
-                                label: 'Overview'
+                        const tabs = [
+                            {
+                                id: 'overview',
+                                tab: {
+                                    label: 'Overview'
+                                },
+                                panel: {
+                                    component: {
+                                        name: OverviewComponent.name(),
+                                        // NB these params are bound here, not in the tabset.
+                                        // TODO: this should be named viewModel since that is what it is...
+                                        params: {
+                                            // view: params.view,
+                                            moduleName: 'moduleName',
+                                            moduleInfo: 'moduleInfo'
+                                        }
+                                    }
+                                }
                             },
-                            panel: {
-                                component: {
-                                    name: OverviewComponent.name(),
-                                    // NB these params are bound here, not in the tabset.
-                                    // TODO: this should be named viewModel since that is what it is...
-                                    params: {
-                                        // view: params.view,
-                                        moduleName: 'moduleName',
-                                        moduleInfo: 'moduleInfo'
+                            {
+                                id: 'spec',
+                                tab: {
+                                    label: 'Spec'
+                                },
+                                panel: {
+                                    component: {
+                                        name: SpecComponent.name(),
+                                        params: {
+                                            moduleName: 'moduleName',
+                                            moduleInfo: 'moduleInfo'
+                                        }
+                                    }
+                                }
+                            },
+                            {
+                                id: 'typesUsing',
+                                tab: {
+                                    label: 'Types'
+                                },
+                                panel: {
+                                    component: {
+                                        name: TypesComponent.name(),
+                                        params: {
+                                            moduleName: 'moduleName',
+                                            moduleInfo: 'moduleInfo'
+                                        }
+                                    }
+                                }
+                            },
+                            {
+                                id: 'includedModules',
+                                tab: {
+                                    label: 'Included modules'
+                                },
+                                panel: {
+                                    component: {
+                                        name: ModulesIncludedComponent.name(),
+                                        params: {
+                                            moduleName: 'moduleName',
+                                            moduleInfo: 'moduleInfo'
+                                        }
+                                    }
+                                }
+                            },
+                            {
+                                id: 'versions',
+                                tab: {
+                                    label: 'Versions'
+                                },
+                                panel: {
+                                    component: {
+                                        name: VersionsComponent.name(),
+                                        params: {
+                                            moduleName: 'moduleName',
+                                            moduleInfo: 'moduleInfo',
+                                            moduleVersions: 'moduleVersions'
+                                        }
                                     }
                                 }
                             }
-                        },
-                        {
-                            id: 'spec',
-                            tab: {
-                                label: 'Spec',
-                            },
-                            panel: {
-                                component: {
-                                    name: SpecComponent.name(),
-                                    params: {
-                                        moduleName: 'moduleName',
-                                        moduleInfo: 'moduleInfo'
-                                    }
-                                }
-                            }
-                        },
-                        {
-                            id: 'typesUsing',
-                            tab: {
-                                label: 'Types',
-                            },
-                            panel: {
-                                component: {
-                                    name: TypesComponent.name(),
-                                    params: {
-                                        moduleName: 'moduleName',
-                                        moduleInfo: 'moduleInfo'
-                                    }
-                                }
-                            }
-                        },
-                        {
-                            id: 'includedModules',
-                            tab: {
-                                label: 'Included modules',
-                            },
-                            panel: {
-                                component: {
-                                    name: ModulesIncludedComponent.name(),
-                                    params: {
-                                        moduleName: 'moduleName',
-                                        moduleInfo: 'moduleInfo'
-                                    }
-                                }
-                            }
-                        },
-                        {
-                            id: 'versions',
-                            tab: {
-                                label: 'Versions',
-                            },
-                            panel: {
-                                component: {
-                                    name: VersionsComponent.name(),
-                                    params: {
-                                        moduleName: 'moduleName',
-                                        moduleInfo: 'moduleInfo',
-                                        moduleVersions: 'moduleVersions'
-                                    }
-                                }
-                            }
-                        }
-                    ];
+                        ];
 
-                    const vm = {
-                        bus: bus,
-                        tabContext: {
-                            moduleName: moduleName,
-                            moduleInfo: moduleInfo,
-                            moduleVersions: moduleVersions
-                        },
-                        tabs: tabs
-                    };
+                        const vm = {
+                            bus: bus,
+                            tabContext: {
+                                moduleName: moduleName,
+                                moduleInfo: moduleInfo,
+                                moduleVersions: moduleVersions
+                            },
+                            tabs: tabs
+                        };
 
-                    container.innerHTML = gen.component({
-                        name: TabsetComponent.name(),
-                        params: {
-                            tabContext: '$root.tabContext',
-                            bus: 'bus',
-                            tabs: 'tabs'
-                        }
-                    }).join('');
+                        container.innerHTML = gen
+                            .component({
+                                name: TabsetComponent.name(),
+                                params: {
+                                    tabContext: '$root.tabContext',
+                                    bus: 'bus',
+                                    tabs: 'tabs'
+                                }
+                            })
+                            .join('');
 
-                    ko.applyBindings(vm, container);
-                });
+                        ko.applyBindings(vm, container);
+                    })
+            );
         }
 
         function stop() {
@@ -203,7 +211,10 @@ define([
         }
 
         return {
-            attach, start, stop, detach
+            attach,
+            start,
+            stop,
+            detach
         };
     }
 
